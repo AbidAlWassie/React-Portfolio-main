ecfc0869d81e86f82aed019b032300f444554ccc
Abid Al Wassie <abidalwassie@outlook.com>
2022-04-15 22:05:43 +0600

    almost done with the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 import { images } from '../../constants'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='navbar'>
       <div className='navbar-logo'>
@@ -11,11 +17,32 @@ const Navbar = () => {
       <ul className='nav-list'>
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className='nav-links' key={`link-${item}`}>
-            <div/>
             <a href={`#${item}`}> {item} </a>
+            <div/>
           </li>
         ))}
       </ul>
+
+      <div className='nav-menu'>
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+          {
+            toggle && (
+              <motion.div 
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.85, ease: 'easeInOut'}}
+              >
+                <HiX onClick={() => setToggle(false)} />
+                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}> {item} </a>
+                  </li>
+                ))}
+              </motion.div>
+            )
+          }
+      </div>
+
     </nav>
   )
 }
