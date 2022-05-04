92107ee024fa6ba88d3e9edaa5830f37fd3e233a
Abid Al Wassie <abidalwassie@outlook.com>
2022-05-04 18:05:33 +0600

    minor changes

--- src/constants/images.jsx
+++ src/constants/images.jsx
@@ -1,7 +1,4 @@
 import profile1 from '../assets/profile1.png';
-import profile2 from '../assets/profile2.png';
-import profile3 from '../assets/profile3.png';
-import circle from '../assets/circle.svg';
 import circle1 from '../assets/circle1.png';
 import circle2 from '../assets/circle2.png';
 import square1 from '../assets/square1.png';
@@ -13,9 +10,6 @@ import research from '../assets/svgs/research.svg';
 import testing from '../assets/svgs/testing.svg';
 import design from '../assets/svgs/design.svg';
 
-import email from '../assets/email.png';
-import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
 import cpp from '../assets/cpp.png';
 import html from '../assets/html.png';
 import css from '../assets/css.png';
@@ -28,35 +22,18 @@ import figma from '../assets/figma.png';
 import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
-import mu5 from '../assets/mu5.png';
 import node from '../assets/node.png';
 import python from '../assets/python.png';
-import redux from '../assets/redux.png';
-import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
 
 import web_development1 from '../assets/web_development1.jpg';
 import web_design1 from '../assets/web_design1.jpg';
 import web_design2 from '../assets/web_design1.jpg';
-import about04 from '../assets/about04.png';
 
-
-import logo from '../assets/logo.png';
-
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
-import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
 
 const data = {
   profile1,
-  profile2,
-  profile3,
-  circle,
   circle1,
   circle2,
   square1,
@@ -68,10 +45,6 @@ const data = {
   testing,
   design,
 
-  logo,
-  email,
-  mobile,
-  api,
   cpp,
   html,
   css,
@@ -84,24 +57,13 @@ const data = {
   figma,
   flutter,
   graphql,
-  mu5,
   node,
   python,
-  redux,
-  sass,
   typescript,
   vue,
   web_development1,
   web_design1,
   web_design2,
-  about04,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
 };
 
 export default data;
